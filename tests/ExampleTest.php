6847475f19d150e020d5f20033176da38f935086
celyes <celyes02@gmail.com>
2020-12-02 19:21:15 +0100

    organize workflow

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase 
+{
+
+
+    public function testThatAllIsOK()
+    {
+        $this->assertTrue(true);
+    }
+
+}
