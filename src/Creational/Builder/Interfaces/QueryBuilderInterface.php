df97c15f636743dd0455e0faa5b8c3539028a90c
celyes <celyes02@gmail.com>
2020-12-07 19:36:21 +0100

    fix issue in builder pattern

--- src/Creational/Builder/Interfaces/QueryBuilderInterface.php
+++ src/Creational/Builder/Interfaces/QueryBuilderInterface.php
@@ -3,7 +3,7 @@
 namespace Patterns\Creational\Builder\Interfaces;
 
 interface QueryBuilderInterface {
-	public function set(): void;
+	public function reset(): void;
 	public function select(string $table, array $fields): self;
 	public function where(string $column, string $operator, string $value): self;
 	public function limit(int $offset, int $count): self;
